9fe965342941013e19a5229c49ccdce7c992ea00
Nuradha Dakshina <IT22269652@my.sliit.lk>
2025-05-04 14:15:24 +0530

    update foodpost

--- Frontend/src/pages/FoodPosts.jsx
+++ Frontend/src/pages/FoodPosts.jsx
@@ -114,7 +114,7 @@ const FoodPostForm = () => {
             htmlFor="title"
             className="block text-sm font-medium text-gray-700 mb-2"
           >
-            Titles
+            Title
           </label>
           <input
             type="text"
